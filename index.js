53d7982868617e315d3a8df616bbe5bd3bf072c8
LUCIANO JOAQUIN VALLEJOS <you@exampl>
2026-06-01 08:57:38 -0300

    Hice todo el armado, hay q empezar a codear

---
+++ index.js
@@ -0,0 +1,13 @@
+import express  from "express"; 
+import cors     from "cors"; 
+import ProvinceRouter from "./src/controllers/province-controller.js"
+
+const app  = express();
+const port = 3000;
+
+app.use(cors());         // Middleware de CORS. 
+app.use(express.json());
+
+app.use("/api/province", ProvinceRouter); 
+
+app.listen(port, () => {     console.log(`Example app listening on port ${port}`) }) 
